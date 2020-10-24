ba0b0d8b0fec360c5f8d41abf66b49473d64903c
Korel Kashri <korelkashri@gmail.com>
2020-10-24 14:32:39 +0300

    make the helper 'to_string' an inline function to resolve conflicts. Separate Stringable to ToStringable => have matching std::to_string method & Stringable => Can be implicitly converted into std::string.

--- include/custom_concepts.hpp
+++ include/custom_concepts.hpp
@@ -8,22 +8,27 @@
 
 namespace std {
     // Stringable concept helper
-    string to_string(string &&val) {
+    inline string to_string(string &&val) {
         return val;
     }
 }
 
 namespace custom_concepts {
     template <typename T>
-    concept Stringable = requires(T t) {
+    concept ToStringable = requires(T t) {
         { std::to_string(t) };
+    };
+
+    template <typename T>
+    concept Stringable = requires(T t) {
+        { (std::string)(t) };
     };
 
     template <typename T>
     concept String = std::is_base_of_v<std::basic_string, T>;
 
     template <typename T>
-    concept StringOrStringable = Stringable<T> || String<T>;
+    concept StringOrStringable = ToStringable<T> || Stringable<T> || String<T>;
 }
 
 #endif
